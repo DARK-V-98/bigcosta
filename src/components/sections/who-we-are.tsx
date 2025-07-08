289212b4a882aeb7320d9855b4ad400a957d430e
Thimira Vishwa <vishwathimira2@gmail.com>
2025-07-08 12:41:25 +0000

    also add this part INTEGRITY. EXPERTISE. LEADERSHIP. Our expertise in es

--- src/components/sections/who-we-are.tsx
+++ src/components/sections/who-we-are.tsx
@@ -11,6 +11,9 @@ export default function WhoWeAre() {
           <p className="text-muted-foreground leading-relaxed">
             Big Costa Construction (Pvt) Ltd is Sri Lanka’s Premier and a leading construction company in Sri Lanka that designs and build luxury houses, villas, hotels, apartments, and office building projects under one roof. We strive higher to deliver value-added products and cost-effective and value driven solutions to our clientele, capitalizing the core competencies of being innovative in the supply of products that’s suited for the market, understanding the need, which defines our brand. We approach business with discipline, consistency, partnerships, and transparency.
           </p>
+          <p className="text-muted-foreground leading-relaxed">
+            INTEGRITY. EXPERTISE. LEADERSHIP. Our expertise in estimating, construction, project management and reconstruction services ensures that our customers receive a quality product at a fair price and in a reasonable time frame. This is accomplished through the selective use of responsible subcontractors, hiring and retaining first class employees, and working for clients that share a belief in open, honest, and direct communication.
+          </p>
         </div>
         <div className="relative h-96 w-full rounded-2xl overflow-hidden shadow-lg group">
           <Image
